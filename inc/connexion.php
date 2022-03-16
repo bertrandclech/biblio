e8eee5ef2aeeb41d578624a2d7c3e713d808434b
renesenses <renesenses@gmail.com>
2022-03-16 10:45:20 +0100

    Maj autoload et cconnecteur

--- inc/connexion.php
+++ inc/connexion.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion à la base de données
-try { $bdd = new PDO('mysql:host=localhost;dbname=garage;charset=utf8', 'root', '', [PDO::ATTR_EMULATE_PREPARES=>false]); }
+try { $bdd = new PDO('mysql:host=localhost;dbname=biblio;charset=utf8', 'root', '', [PDO::ATTR_EMULATE_PREPARES=>false]); }
 // echo "Connecté à la base";
 catch (Exception $e) { echo $e->getMessage(); }
 // Si le try échoue, on capture l'erreur et on l'affiche
--- php/autoload.php
+++ php/autoload.php
@@ -2,5 +2,5 @@
 
 // Autoload
 spl_autoload_register(function ($className) {
-    require_once    'src' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $className . '.class.php';
+    require_once    'classes' . DIRECTORY_SEPARATOR . $className . '.class.php';
 });
